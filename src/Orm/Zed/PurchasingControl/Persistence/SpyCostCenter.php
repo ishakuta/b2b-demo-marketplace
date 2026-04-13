226fb36c32e5d5af95226b3528848150dfd64f29
denyssokolov <denys.sokolov@spryker.com>
2026-04-13 16:29:39 +0200

    CC-38071: Add Purchasing Control feature with cost center and budget management.

---
+++ src/Orm/Zed/PurchasingControl/Persistence/SpyCostCenter.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orm\Zed\PurchasingControl\Persistence;
+
+use Orm\Zed\PurchasingControl\Persistence\Base\SpyCostCenter as BaseSpyCostCenter;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_cost_center' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class SpyCostCenter extends BaseSpyCostCenter
+{
+
+}
